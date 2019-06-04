373ba366417de213c0817e7b7f444530d5c25d1e
David Davis <davidjamesdavis.djd@gmail.com>
2019-06-04 15:12:17 -0700

    gatsby sanity monorepo

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,3 +1,8 @@
+require('dotenv').config()
+const {
+  api: { projectId, dataset }
+} = requireConfig('../cms/sanity.json')
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
@@ -5,6 +10,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    'gatsby-plugin-postcss',
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -27,8 +33,40 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: projectId,
+        dataset: dataset,
+        // To enable preview of drafts, copy .env-example into .env,
+        // and add a token with read permissions
+        token: process.env.SANITY_TOKEN,
+        watchMode: true,
+        overlayDrafts: true
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
+}
+
+/**
+ * We're requiring a file in the studio folder to make the monorepo
+ * work "out-of-the-box" with project credentials.
+ */
+function requireConfig(path) {
+  try {
+    return require('../cms/sanity.json')
+  } catch (e) {
+    console.error(
+      'Failed to require sanity.json. Fill in projectId and dataset name manually in gatsby-config.js'
+    )
+    return {
+      api: {
+        projectId: process.env.SANITY_PROJECT_ID || '',
+        dataset: process.env.SANITY_DATASET || ''
+      }
+    }
+  }
 }
