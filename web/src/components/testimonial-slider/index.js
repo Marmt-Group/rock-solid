f72c0cb81556ba651686684c03450340a9fcc150
David Davis <davidjamesdavis.djd@gmail.com>
2019-08-06 20:59:55 -0700

    module require window fix

--- web/src/components/testimonial-slider/index.js
+++ web/src/components/testimonial-slider/index.js
@@ -2,34 +2,39 @@ import React from 'react'
 import './style.scss'
 import Flickity from 'react-flickity-component'
 
-const TestimonialSlider = ({ testimonials }) => (
-    <section className="testimonials-slider text-center">
-        <div className="container">
-            <div className="row justify-content-sm-center">
-                <div className="col-sm-10">
-                    <div className="slider" data-paging="true">
-                        <div className="slides">
-                            
-                            <Flickity
-                                elementType={'div'} // default 'div'
-                                options={{}} // takes flickity options {}
-                            >
-                            {testimonials.nodes && testimonials.nodes.map((testimonial, index) => (
-                            <div className="slide" key={index}>
-                                <div className="testimonial">
-                                    <blockquote>“{testimonial.quote}”</blockquote>
-                                    <h5>{testimonial.person}</h5>
+const TestimonialSlider = ({ testimonials }) => {
+
+    if (typeof window !== 'undefined') {
+        return (
+            <section className="testimonials-slider text-center">
+                <div className="container">
+                    <div className="row justify-content-sm-center">
+                        <div className="col-sm-10">
+                            <div className="slider" data-paging="true">
+                                <div className="slides">
+                                    
+                                    <Flickity
+                                        elementType={'div'} // default 'div'
+                                        options={{}} // takes flickity options {}
+                                    >
+                                    {testimonials.nodes && testimonials.nodes.map((testimonial, index) => (
+                                    <div className="slide" key={index}>
+                                        <div className="testimonial">
+                                            <blockquote>“{testimonial.quote}”</blockquote>
+                                            <h5>{testimonial.person}</h5>
+                                        </div>
+                                    </div>
+                                    ))}
+                                    </Flickity>
+                                    
                                 </div>
                             </div>
-                            ))}
-                            </Flickity>
-                            
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </section>
-)
+            </section>
+        )
+    }
+}
 
 export default TestimonialSlider
