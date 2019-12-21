7e00fc10c9b44d7b36cfcd15d67add9e06a8b292
David Davis <3498223+david-j-davis@users.noreply.github.com>
2019-12-20 17:01:07 -0800

    mods to video view

--- web/src/components/video-player/index.js
+++ web/src/components/video-player/index.js
@@ -16,21 +16,19 @@ const VideoPlayer = ({ data, options }) => {
         width: '100%'
     }
 
-    let actual = Object.assign({}, defaults, options);
-
     return (
         <div>
             <SanityMuxPlayer
                 assetDocument={data}
-                autoload={actual.autoload}
-                autoplay={actual.autoplay}
-                showControls={actual.showControls}
-                loop={actual.loop}
-                muted={actual.muted}
-                className={actual.className}
-                style={actual.style}
-                height={actual.height}
-                width={actual.width}
+                autoload={defaults.autoload}
+                autoplay={defaults.autoplay}
+                showControls={defaults.showControls}
+                loop={defaults.loop}
+                muted={defaults.muted}
+                className={defaults.className}
+                style={defaults.style}
+                height={defaults.height}
+                width={defaults.width}
             />
         </div>
     )
