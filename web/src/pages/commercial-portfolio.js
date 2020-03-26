d7594b4d1be024d7e0a31ff9aff7e15e9f4ead18
David Davis <daviddavis@pop-os.localdomain>
2020-03-25 20:20:19 -0700

    init video page

---
+++ web/src/pages/commercial-portfolio.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import GraphQLErrorList from '../components/graphql-error-list'
+import SEO from '../components/seo'
+import Layout from '../components/layout'
+import HeroNarrow from '../components/hero-narrow'
+import ProjectPreviewGrid from '../components/project-preview-grid'
+import TestimonialSlider from '../components/testimonial-slider'
+
+export const query = graphql`
+  query CommercialPageQuery {
+    hero: file(relativePath: { eq: "pool-deck-3.jpeg" }) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 4160) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+
+    projects_commercial: allSanityProject(sort: {order: DESC, fields: publishedAt}, filter: {categories: {elemMatch: {title: {eq: "Commercial"}}}}) {
+      nodes {
+        title
+        publishedAt
+        _id
+        mainImage {
+          crop {
+            _key
+            _type
+            bottom
+            left
+            right
+            top
+          }
+          hotspot {
+            _key
+            _type
+            height
+            width
+            x
+            y
+          }
+          asset {
+            _id
+            assetId
+          }
+          alt
+        }
+      }
+    }
+
+    testimonials: allSanityTestimonials {
+      nodes {
+        person
+        quote
+      }
+    }
+
+  }
+`
+
+const CommercialPage = props => {
+  const { data, errors } = props
+  const hero = (data || {}).hero
+  const testimonials = (data || {}).testimonials
+  const projectNodes = {}
+
+  projectNodes.commercial = (data || {}).projects_commercial
+
+  if (errors) {
+    return (
+      <Layout>
+        <GraphQLErrorList errors={errors} />
+      </Layout>
+    )
+  }
+
+  return (
+    <Layout>
+      <SEO title={'Projects Portfolio'} />
+      <HeroNarrow imgUrl={hero} header={'If you imagine it, we can do it.'} lead={'Not sure what you’re looking for? No problem! We’re happy to help you design something beautiful. Take a look through our galleries to see some of our favorite designs.'} />
+        {projectNodes && (
+          <ProjectPreviewGrid
+            gallery={projectNodes}
+          />
+        )}
+      <TestimonialSlider testimonials={testimonials} />
+    </Layout>
+  )
+}
+
+export default CommercialPage
